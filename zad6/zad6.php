0ce066b5fa515a787a1d0cbfd1bac2c4f214cbbe
Oleg19 <oshkinoleg@mail.ru>
2015-02-23 19:04:52 +0300

    2 commit

---
+++ zad6/zad6.php
@@ -0,0 +1,29 @@
+<! Ваши задание будет создание создание сокращенного варианта ФИО, т. е.
+<! вводим: Иванов Иван Петрович и нам выводит: Иванов И. П.
+<!DOCTYPE html>
+<html>
+<head lang="en"> <meta charset="UTF-8">
+	<title>Задание №6</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<form action="index.php" method="post" enctype="multipart/form-data">
+
+	<header class="container">
+		<div class="backgr">
+			<p> Введите ФИО полностью </p>
+			<input type="text" name="firstName"/>
+			<input type="text" name="Name"/>
+			<input type="text" name="lastName"/>
+			<input type="submit" value=" Ввод " />
+
+
+		</div>
+	</header>
+
+</form>
+
+</body>
+</html>
+
+
