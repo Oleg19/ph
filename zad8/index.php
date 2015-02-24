2d1ab9c67b68993f7fe0dc4de855163161b57fb2
Oleg19 <oshkinoleg@mail.ru>
2015-02-24 22:27:17 +0300

    Доработка

--- zad8/index.php
+++ zad8/index.php
@@ -1,20 +1,28 @@
 <?php
 function __autoload($class) {
-    require __DIR__  . '\\' . $class . '.html';
-    //require __DIR__  . '/view/' .  $class . '.php';
+    require __DIR__  . '\\' . $class . '.php';
+
 }
 
 $b = $_POST['b'];
 $i = fmod($b , 5);
-echo $i;
-if ($i == 0 || $i <= 3)
-    {
-        $a = new Svetofor('Зелёный');
-        //var_dump($a);
-        return $a;
-    }
+
+if ($i == 0 || $i == 4 )
+{
+    $view = new View;
+    $view->items = $a = 'Красный';
+    include('SvetoforRed.html');
+
+}
 else
-    {
-        $a = new Svetofor('Красный');
-        return $a;
-    }
+{
+    $view = new View;
+    $view->items = $a = 'Зелёный';
+    include('SvetoforGreen.html');
+
+}
+
+
+
+
+
